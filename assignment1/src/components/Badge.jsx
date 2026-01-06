119549b088b84b6636e60c17fc4f1af81c756967
fataneh-mow <fataneh.mow@gmail.com>
2026-01-06 14:25:17 +0430

    features added

--- assignment1/src/components/Badge.jsx
+++ assignment1/src/components/Badge.jsx
@@ -1,6 +1,15 @@
+import '../index.css'
 export default function Badge ({label, value}) {
     return (
-        <div>
+        <div className="flex flex-cols-2 gap-4 justify-between items-center mx-auto w-1/4 my-2 bg-gray-50 p-3 rounded-md shadow-md hover:bg-gray-100">
+            <div className="text-xl text-indigo-600">
+                {label}
+            </div>
+            <div className="text-indigo-500 text-md mt-[4px]">
+                <p className="text-center hover:bg-white px-4 py-2 rounded-md shinny-red-border transition ease-in duration-200">
+                    {value}
+                </p>
+            </div>
         </div>
     )
 }
---
+++ assignment1/src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header () {
+    return (
+        <div 
+            className="text-center flex justify-center items-center p-12 my-6 bg-indigo-500 rounded-md shadow-sm mx-auto hover:bg-indigo-700 transiiton ease-in duration-200"
+        >
+            <h1 className="text-2xl font-bold text-white">
+                React Week 1 - Assignment 1
+            </h1>
+        </div>
+    )
+}
