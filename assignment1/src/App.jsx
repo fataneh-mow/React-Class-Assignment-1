8d20936332ebeb17747bc7d556dbed3ea71dce93
fataneh-mow <fataneh.mow@gmail.com>
2026-01-08 12:08:40 +0430

    refactored code

--- assignment1/src/App.jsx
+++ assignment1/src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 export default function App () {
   return (
     <div className="grid grid-rows my-12">
-      <Header></Header>
+      <Header title={"Week one - Assignment one"}></Header>
       <Badge label="Course" value="React" />
       <Badge label="Week" value="One" />
       <Badge label="Topic" value="Props" />
