ea1004086a3f74a0466085343815fdb8ae3abaca
fataneh-mow <fataneh.mow@gmail.com>
2026-01-08 12:07:55 +0430

    feat: Header component became dynamic and reusable

--- assignment1/src/components/Header.jsx
+++ assignment1/src/components/Header.jsx
@@ -1,10 +1,10 @@
-export default function Header () {
+export default function Header ({title}) {
     return (
         <div 
             className="text-center flex justify-center items-center p-12 my-6 bg-indigo-500 rounded-md shadow-sm mx-auto hover:bg-indigo-700 transiiton ease-in duration-200"
         >
             <h1 className="text-2xl font-bold text-white">
-                React Week 1 - Assignment 1
+                {title}
             </h1>
         </div>
     )
